7e8e06e04db2a7fb0b92223eb2fd37d3751058e3
Conan Li <conanlics@gmail.com>
2017-07-29 21:51:16 -0400

    README

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
 
 here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='command_streamer',
-    version='0.0.1',
+    version='0.0.2',
     description='Stream command output using asyncio',
     url='https://github.com/conanfanli/command-streamer',
     packages=find_packages(exclude=['tests*']),
